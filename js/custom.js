8562d65d39847d8d1f4dfd87ee6485fb4c6ad9d9
C M Khaled Saifullah <khaledkucse@Cs-MacBook-Pro.local>
2020-10-14 18:51:53 -0600

    contem=nt updated

--- js/custom.js
+++ js/custom.js
@@ -49,6 +49,27 @@ jQuery(function($) {
 // prettyphoto
 
  $("a[data-rel^='prettyPhoto']").prettyPhoto();
+
+
+ $(window).load(function() {
+    'use strict';
+    var $portfolio_selectors = $('.portfolio-filter >li>a');
+    var $portfolio = $('.portfolio-items');
+    $portfolio.isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+
+    $portfolio_selectors.on('click', function() {
+        $portfolio_selectors.removeClass('active');
+        $(this).addClass('active');
+        var selector = $(this).attr('data-filter');
+        $portfolio.isotope({
+            filter: selector
+        });
+        return false;
+    });
+});
  
     
  
